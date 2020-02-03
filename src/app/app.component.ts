da456f10a54e314ab63703beed82ff7c80843ac1
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-02-03 19:05:25 +0100

    Property binding exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  blackDiv = 'black';
+  yellowDiv = 'yellow';
+  colorSwitch = 'green';
+
+
+  logo = '/assets/angular.png';
+  blackLogo = '/assets/angular_solidBlack.png';
+  text = 'Zadanie wykonane!';
+  htmlText = '<h2>Anglar jest fajny</h2><blockquote>Ale trzeba się dużo uczyć</blockquote>';
+  isDisabled = true;
+  changeColor = false;
+
+}
